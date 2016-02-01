ad0379e391d470e3dcd420fe217cd4c57dd564d6
Dannon <dannon99@gmail.com>
2016-02-01 20:45:58 +0530

    reverted ScreenContainer

--- app/src/main/java/com/virtuotek/overseer/ScreenContainer.java
+++ app/src/main/java/com/virtuotek/overseer/ScreenContainer.java
@@ -1,6 +1,6 @@
 package com.virtuotek.overseer;
 
-import android.app.Activity;
+import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
@@ -9,7 +9,7 @@ import android.widget.FrameLayout;
  * Main screen container. Will only display one active screen at a time. Place this is your main activity layout
  */
 public class ScreenContainer extends FrameLayout {
-    public ScreenContainer(Activity activity, AttributeSet attrs) {
-        super(activity, attrs, 0);
+    public ScreenContainer(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
     }
 }
